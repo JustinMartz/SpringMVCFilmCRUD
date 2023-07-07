8a30c8c2bd5afac54329264b6e3a001e19090f26
Dean Coffman <deansdetail@gmail.com>
2023-07-07 18:31:14 -0400

    full stake SMOKE test

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -2,6 +2,8 @@ package com.skilldistillery.film.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 import com.skilldistillery.film.data.FilmDAO;
 
@@ -9,5 +11,10 @@ import com.skilldistillery.film.data.FilmDAO;
 public class FilmController {
 	@Autowired
 	private FilmDAO filmDAO;
-
+@RequestMapping(path = {"/", "home.do"} )
+	public ModelAndView goHome() {
+		ModelAndView mv = new ModelAndView("WEB-INF/home.jsp");
+		
+		return mv;
+	}
 }
